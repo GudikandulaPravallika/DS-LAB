9ab1a8179ceeff0c60fe93912e1ef8512d311e55
GudikandulaPravallika <21bq1a4250@vvit.net>
2022-11-22 20:13:41 +0530

    Modified

--- insertionsort.py
+++ insertionsort.py
@@ -3,9 +3,10 @@ def insertionsort(lt):
         j=i-1
         next_element=lt[i]
         while lt[j]>next_element and j>=0:
-            lt[j+1]==lt[j]
+            lt[j+1]=lt[j]
             j=j-1
         lt[j+1]=next_element
+    return lt
 l=[]
 n=int(input("enter the size of list"))
 for i in range(n):
